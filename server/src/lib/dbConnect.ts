e605ddc8a1042eb095a57673c366346e098fd973
Hetav21 <shahhetav2106@gmail.com>
2024-04-25 23:10:59 +0530

    Init Project

---
+++ server/src/lib/dbConnect.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate())
